ebeadd54214d798ac171b068557fd0d5ea5c52f6
svolkov-amd <stanislav.volkov@amd.com>
2026-03-25 22:35:33 +0000

    dummy test

--- projects/rocprofiler-compute/src/lib/rocprofiler_compute_tool/test/test_rocprofiler_compute_tool.cpp
+++ projects/rocprofiler-compute/src/lib/rocprofiler_compute_tool/test/test_rocprofiler_compute_tool.cpp
@@ -1,8 +1,20 @@
-#include <gtest/gtest.h>
 #include "rocprofiler_compute_tool.h"
 
-int main(int argc, char **argv) {
+#include <gtest/gtest.h>
+
+class TestRocprofilerComputeTool : public ::testing::Test
+{
+protected:
+
+};
+
+TEST_F(TestRocprofilerComputeTool, TestExample)
+{
+    EXPECT_EQ(1 + 1, 2);
+} 
+
+int main(int argc, char** argv)
+{
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
