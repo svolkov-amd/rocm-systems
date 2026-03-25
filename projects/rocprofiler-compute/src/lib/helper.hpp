de099beb4383376f06f1e3d922f8d9086aca4b06
svolkov-amd <stanislav.volkov@amd.com>
2026-03-25 21:28:16 +0000

    code forrmatting

--- projects/rocprofiler-compute/src/lib/helper.hpp
+++ projects/rocprofiler-compute/src/lib/helper.hpp
@@ -25,11 +25,11 @@
 #include <string>
 #include <vector>
 
-namespace helper_utils {
+namespace helper_utils
+{
 
-std::string truncate_name(std::string_view name);
-std::string cxa_demangle(std::string_view _mangled_name, int *_status);
-std::vector<std::string> split_by_regex(const std::string &s,
-                                        const std::string &regex_pattern);
+std::string              truncate_name(std::string_view name);
+std::string              cxa_demangle(std::string_view _mangled_name, int* _status);
+std::vector<std::string> split_by_regex(const std::string& s, const std::string& regex_pattern);
 
-} // namespace helper_utils
+}  // namespace helper_utils
