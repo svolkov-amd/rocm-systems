c4b5c14ac60e141e425a26b8029a18d86e3cb038
svolkov-amd <stanislav.volkov@amd.com>
2026-03-25 23:32:22 +0000

    added function to override InputParameters

--- projects/rocprofiler-compute/src/lib/rocprofiler_compute_tool/rocprofiler_compute_tool.cpp
+++ projects/rocprofiler-compute/src/lib/rocprofiler_compute_tool/rocprofiler_compute_tool.cpp
@@ -22,13 +22,13 @@
 
 #include "helper.hpp"
 #include "input_parameters.h"
+#include "rocprofiler_compute_tool.h"
 #include "sdk_wrapper.h"
 
 #include <unistd.h>
 
 #include <fstream>
 #include <iostream>
-#include <memory>
 #include <mutex>
 #include <set>
 #include <shared_mutex>
@@ -39,7 +39,13 @@
 
 using namespace rocprof_compute_tool;
 
-EnvInputParameters g_input_parameters;
+std::shared_ptr<InputParameters> g_input_parameters = std::make_shared<EnvInputParameters>();
+
+void test_knobs::set_input_parameters(std::shared_ptr<InputParameters> input_parameters)
+{
+    g_input_parameters = input_parameters;
+}
+
 
 #define ROCPROFILER_CALL(result, msg)                                                                  \
     {                                                                                                  \
@@ -613,7 +619,7 @@ std::unique_ptr<tool_data_t> create_tool_data(rocprofiler_client_id_t* /*id*/)
 
     // Require ROCPROF_OUTPUT_PATH to be set, otherwise error out
     std::string filename;
-    const char* output_path = g_input_parameters.get_output_path();
+    const char* output_path = g_input_parameters->get_output_path();
     if (!output_path || !*output_path)
     {
         throw std::runtime_error("ROCPROF_OUTPUT_PATH environment variable must be set");
@@ -629,20 +635,20 @@ std::unique_ptr<tool_data_t> create_tool_data(rocprofiler_client_id_t* /*id*/)
     // Store ROCPROF env. vars. in tool_data
 
     // ROCPROF_COUNTERS env. var. is a string like "pmc: counter1 counter2 ..."
-    if (const char* v = g_input_parameters.get_requested_counters())
+    if (const char* v = g_input_parameters->get_requested_counters())
         tool_data->requested_counters = v;
 
-    if (const char* v = g_input_parameters.get_iteration_multiplexing_mode())
+    if (const char* v = g_input_parameters->get_iteration_multiplexing_mode())
         tool_data->iteration_multiplexing_mode = iteration_multiplexing_mode(v);
 
     // ROCPROF_KERNEL_FILTER_INCLUDE_REGEX env. var. is a regex string like
     // kernel_name_1|kernel_name_2|... Used to collect counters only for kernels
     // with names matching the regex
-    if (const char* v = g_input_parameters.get_kernel_filter_include_regex())
+    if (const char* v = g_input_parameters->get_kernel_filter_include_regex())
         tool_data->kernel_filter_include_regex = v;
 
     // ROCPROF_KERNEL_FILTER_RANGE env. var. is a string like "[4,7-9,...]"
-    if (const char* v = g_input_parameters.get_kernel_filter_range())
+    if (const char* v = g_input_parameters->get_kernel_filter_range())
     {
         // Remove square brackets at the ends if present
         std::string v_str = v;
--- projects/rocprofiler-compute/src/lib/rocprofiler_compute_tool/rocprofiler_compute_tool.h
+++ projects/rocprofiler-compute/src/lib/rocprofiler_compute_tool/rocprofiler_compute_tool.h
@@ -1,7 +1,15 @@
 #pragma once
+#include "input_parameters.h"
 #include "sdk_wrapper.h"
+
+#include <memory>
 
 rocprofiler_tool_configure_result_t* rocprofiler_configure(uint32_t                 version,
                                                            const char*              runtime_version,
                                                            uint32_t                 priority,
                                                            rocprofiler_client_id_t* id);
+
+namespace rocprof_compute_tool::test_knobs
+{
+void set_input_parameters(std::shared_ptr<InputParameters> parameters);
+}
